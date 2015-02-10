67366accd26cb430d767f346d6ddc6198d976f9c
Adam Panzer <apanzerj@gmail.com>
2015-02-09 19:43:06 -0800

    TableView laid down. AutoLayout Activated.

--- YelpMeOutHere/ViewController.swift
+++ YelpMeOutHere/ViewController.swift
@@ -8,16 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var venueList: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        venueList.dataSource = self
+        venueList.delegate = self
+        venueList.layer.borderWidth = 2
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
 
 
